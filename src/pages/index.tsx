d37637b45e614c6cfb3867af304b85a7afcaf77f
degui1 <gui.denez56@gmail.com>
2024-08-12 09:41:24 -0300

    stitches & keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,51 @@
+import 'keen-slider/keen-slider.min.css'
+
+import { useKeenSlider } from 'keen-slider/react'
+import Image from 'next/image'
+
+import camiseta01 from '../assets/camisetas/1.png'
+import camiseta02 from '../assets/camisetas/2.png'
+import camiseta03 from '../assets/camisetas/3.png'
+import { HomeContainer, Product } from '../styles/pages/home'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <h1>Hello world</h1>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta01} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta02} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta03} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta03} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
